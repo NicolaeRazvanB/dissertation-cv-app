b0f3e494799ef53cdf535f9251cb0a8ba1d74bae
NicolaeRazvanB <nicolaerazvan.burca@gmail.com>
2024-04-03 21:40:17 +0300

    adding changes to navbar component

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -1,8 +1,8 @@
+import { useContext } from "react";
+import { Button, Navbar, Container, Row, Col, Nav } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { Logout } from "../context/AuthActions";
-import { useContext } from "react";
-import { Button, Navbar, Container, Row, Col } from "react-bootstrap";
-import { useLocation, useNavigate } from "react-router-dom";
 
 const NavbarComponent = () => {
   const { dispatch } = useContext(AuthContext);
@@ -12,14 +12,19 @@ const NavbarComponent = () => {
   const handleLogout = () => {
     localStorage.removeItem("userInfo");
     dispatch(Logout());
+    navigate("/login");
   };
 
   const handleBack = () => {
     navigate(-1); // Go back to the previous page
   };
 
+  const handleHomeClick = () => {
+    navigate("/"); // Navigate to the home page
+  };
+
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" variant="dark" sticky="top">
       <Container fluid>
         <Row className="w-100">
           <Col xs={4} className="d-flex justify-content-start">
@@ -30,7 +35,13 @@ const NavbarComponent = () => {
             )}
           </Col>
           <Col xs={4} className="d-flex justify-content-center">
-            <Navbar.Brand>MagickalResume</Navbar.Brand>
+            {/* Wrap the Navbar.Brand in a div or Nav.Link to make it clickable */}
+            <Nav.Link
+              onClick={handleHomeClick}
+              style={{ color: "white", cursor: "pointer" }}
+            >
+              <Navbar.Brand>MagickalResume</Navbar.Brand>
+            </Nav.Link>
           </Col>
           <Col xs={4} className="d-flex justify-content-end">
             <Button variant="outline-light" onClick={handleLogout}>
